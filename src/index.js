c315346bedfda5a7305be5795669824db15bbad9
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2024-04-03 10:42:02 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.get('/hello', (req, res) => {
+  res.send('Hello Node!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
